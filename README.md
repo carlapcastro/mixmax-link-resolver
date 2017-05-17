44d678a59cfaeb1d7864c7050436ae9f8447064d
carlapcastro <carlacastro47@gmail.com>
2017-05-16 23:12:12 -0700

    adding link resolvers

---
+++ README.md
@@ -0,0 +1,8 @@
+# Spotify Link Resolver
+
+This repo hold the Mixmax link resolver integration for Spotify links and URIs. 
+
+## Running locally
+
+1. Install using `npm install`
+2. Run using `npm start`
---
+++ api/resolver.js
@@ -0,0 +1,28 @@
+var request = require('request');
+var _ = require('underscore');
+
+
+// The API that returns the in-email representation.
+module.exports = function(req, res) {
+  var url = req.query.url.trim();
+  var uri;
+
+  // Spotify formats for urls or uris:
+  var isUri = url.match(/spotify:.*:([a-zA-Z0-9]{22})$/);
+  var isUrl = url.match(/spotify\.com\/.*\/([a-zA-Z0-9]{22})/);
+
+  if (isUri) {
+    uri = url;
+  } else if (isUrl) {
+    uri = 'spotify' + url.substring(url.indexOf('.com') + 4).split('/').join(':');
+  } else {
+    res.status(400).send('Invalid URL format');
+    return;
+  }
+
+  var html = '<iframe src="https://open.spotify.com/embed?uri=' + uri
+    + '" width="300" height="380" frameborder="0" allowtransparency="true"></iframe>';
+  res.json({
+    body: html
+  });
+};
